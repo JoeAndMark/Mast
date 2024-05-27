ef16e459498963f8e80b716d899cf78e8e9b2b63
Joe And Mark <joe@macbook-pro.mshome.net>
2024-05-28 01:21:34 +0800

    添加关闭提示功能

--- mast/main.py
+++ mast/main.py
@@ -12,56 +12,93 @@ class MainWindow(QMainWindow):
         super(MainWindow, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        self.model = QFileSystemModel()\
+        self.model = QFileSystemModel()
 
         self.supportFileType = "Text Files (*.txt);;Markdown Files (*.md);;LaTeX Files (*.tex);;Typst Files (*.typ)"
         
         self.defaultFileDir = os.path.expanduser("~\\Documents")
+        self.currentFileDir = self.defaultFileDir
+        self.currentFilePath = None
 
-        self.currentFileDir = self.defaultFileDir # 刚运行软件时，设置为默认文件路径
         self.model.setRootPath(self.currentFileDir)
         self.ui.treeView.setModel(self.model)
         self.ui.treeView.setRootIndex(self.model.index(self.model.rootPath()))
 
         self.connectSlot()
-    
+
+    def connectSlot(self):
+        self.ui.fileOpen.triggered.connect(self.fileOpen)
+        self.ui.fileNew.triggered.connect(self.fileNew)
+        self.ui.fileOpenFolder.triggered.connect(self.fileOpenFolder)
+        self.ui.fileNewWindow.triggered.connect(self.fileNewWindow)
+        self.ui.fileSave.triggered.connect(self.fileSave)
+        self.ui.fileSaveAs.triggered.connect(self.fileSaveAs)
+        self.ui.fileMoveTo.triggered.connect(self.fileMoveTo)
+
+        self.ui.editUndo.triggered.connect(self.ui.textEdit.undo)
+        self.ui.editRedo.triggered.connect(self.ui.textEdit.redo)
+        self.ui.editCopy.triggered.connect(self.ui.textEdit.copy)
+        self.ui.editCut.triggered.connect(self.ui.textEdit.cut)
+        self.ui.editPaste.triggered.connect(self.ui.textEdit.paste)
+        self.ui.editFind.triggered.connect(self.editFind)
+        self.ui.editJumpToTop.triggered.connect(self.editJumpToTop)
+        self.ui.editJumpToBottom.triggered.connect(self.editJumpToBottom)
+        self.ui.editJumpToSelection.triggered.connect(self.editJumpToSelection)
+        self.ui.editJumpToLineStart.triggered.connect(self.editJumpToLineStart)
+        self.ui.editJumpToLineEnd.triggered.connect(self.editJumpToLineEnd)
+        self.ui.editReplace.triggered.connect(self.editReplace)
+
+        self.ui.compileLaTeX.triggered.connect(self.compileLaTeX)
+        # self.ui.compileTypst.triggered.connect(self.compileTypst) # 拼写错误
+
+        self.ui.helpAbout.triggered.connect(self.helpAbout)
+
     def fileOpen(self):
         file, ok = QFileDialog.getOpenFileName(self, "Open", self.currentFileDir, self.supportFileType)
         if ok:
+            self.currentFilePath = file
             with open(file, 'r') as f:
                 content = f.read()
             self.ui.textEdit.setText(content)
-        self.ui.statusBar.showMessage(file)
+            self.ui.statusBar.showMessage(f"Opened: {file}")
 
-    def fileOpenFolder(self):
-        folder = QFileDialog.getExistingDirectory(self, "Open Folder", self.currentFileDir)
-        self.ui.statusBar.showMessage(folder)
-    
     def fileNew(self):
         file, ok = QFileDialog.getSaveFileName(self, "New File", "", self.supportFileType)
         if ok:
+            self.currentFilePath = file
             with open(file, 'w') as f:
                 f.write('')
-            self.ui.statusBar.showMessage(file)
-    
+            self.ui.textEdit.clear()
+            self.ui.statusBar.showMessage(f"Created: {file}")
+
     def fileNewWindow(self):
         new_window = MainWindow()
         new_window.show()
         self.new_window = new_window
 
     def fileSave(self):
-        file, ok = QFileDialog.getSaveFileName(self, "Save File", "", self.supportFileType)
-        if ok:
-            with open(file, 'w') as f:
+        if self.currentFilePath:
+            with open(self.currentFilePath, 'w') as f:
                 f.write(self.ui.textEdit.toPlainText())
-            self.ui.statusBar.showMessage(file)
+            self.ui.statusBar.showMessage(f"Saved: {self.currentFilePath}")
+        else:
+            self.fileSaveAs()
 
     def fileSaveAs(self):
         file, ok = QFileDialog.getSaveFileName(self, "Save File As", "", self.supportFileType)
         if ok:
+            self.currentFilePath = file
             with open(file, 'w') as f:
                 f.write(self.ui.textEdit.toPlainText())
-            self.ui.statusBar.showMessage(file)
+            self.ui.statusBar.showMessage(f"Saved As: {file}")
+
+    def fileOpenFolder(self):
+        folder = QFileDialog.getExistingDirectory(self, "Open Folder", self.currentFileDir)
+        if folder:
+            self.currentFileDir = folder
+            self.model.setRootPath(folder)
+            self.ui.treeView.setRootIndex(self.model.index(folder))
+            self.ui.statusBar.showMessage(f"Opened Folder: {folder}")
 
     def fileMoveTo(self):
         sourceFile, ok = QFileDialog.getOpenFileName(self, "Select File", "", self.supportFileType)
@@ -69,11 +106,11 @@ class MainWindow(QMainWindow):
             destinationFolder = QFileDialog.getExistingDirectory(self, "Select Destination Folder", "")
             if destinationFolder:
                 destinationFile = shutil.move(sourceFile, destinationFolder)
-                self.ui.statusBar.showMessage(destinationFile)
+                self.ui.statusBar.showMessage(f"Moved to: {destinationFile}")
 
     def editFind(self):
         text, ok = QInputDialog.getText(self, 'Find', 'Enter text:')
-        if ok:
+        if ok and text:
             cursor = self.ui.textEdit.textCursor()
             format = QTextCharFormat()
             format.setBackground(QColor('yellow'))
@@ -88,15 +125,19 @@ class MainWindow(QMainWindow):
                 self.ui.textEdit.setExtraSelections(extra_selections)
                 start_pos = self.ui.textEdit.textCursor().position()
 
+    def editReplace(self):
+        cursor = self.ui.textEdit.textCursor()
+        if cursor.hasSelection():
+            text, ok = QInputDialog.getText(self, 'Replace Text', 'Enter new text:')
+            if ok:
+                cursor.insertText(text)
+                self.ui.textEdit.setTextCursor(cursor)
+
     def editJumpToTop(self):
-        cursor = self.ui.textEdit.textCursor()
-        cursor.movePosition(QTextCursor.Start)
-        self.ui.textEdit.setTextCursor(cursor)
-    
+        self.ui.textEdit.moveCursor(QTextCursor.Start)
+
     def editJumpToBottom(self):
-        cursor = self.ui.textEdit.textCursor()
-        cursor.movePosition(QTextCursor.End)
-        self.ui.textEdit.setTextCursor(cursor)
+        self.ui.textEdit.moveCursor(QTextCursor.End)
 
     def editJumpToSelection(self):
         cursor = self.ui.textEdit.textCursor()
@@ -106,89 +147,54 @@ class MainWindow(QMainWindow):
             self.ui.textEdit.setTextCursor(cursor)
     
     def editJumpToLineStart(self):
-        cursor = self.ui.textEdit.textCursor()
-        cursor.movePosition(QTextCursor.StartOfLine)
-        self.ui.textEdit.setTextCursor(cursor)
+        self.ui.textEdit.moveCursor(QTextCursor.StartOfLine)
 
     def editJumpToLineEnd(self):
-        cursor = self.ui.textEdit.textCursor()
-        cursor.movePosition(QTextCursor.EndOfLine)
-        self.ui.textEdit.setTextCursor(cursor)
+        self.ui.textEdit.moveCursor(QTextCursor.EndOfLine)
 
     def compileLaTeX(self):
-        filePath = self.ui.textEdit.toPlainText()
-        directory, file  = os.path.split(filePath)
-        baseName, ext = os.path.splitext(file)
-
-        if ext.lower() != '.tex':
+        if not self.currentFilePath or not self.currentFilePath.endswith('.tex'):
             QMessageBox.warning(self, "Warning", "The file is not a LaTeX file.")
             return
         
-        command = [
-            'pdflatex',
-            '-interaction=nonstopmode',
-            file
-        ]
+        directory, file = os.path.split(self.currentFilePath)
+        command = ['pdflatex', '-interaction=nonstopmode', file]
 
         try:
             process = subprocess.Popen(command, cwd=directory)
             process.wait()
+            self.ui.statusBar.showMessage(f"Compiled LaTeX: {file}")
         except Exception as e:
             QMessageBox.critical(self, "Error", str(e))
 
     def compileTypst(self):
-        filePath = self.ui.textEdit.toPlainText()
-        directory, file = os.path.split(filePath)
-        baseName, ext = os.path.splitext(file)
-
-        if ext.lower() != '.typ':
+        if not self.currentFilePath or not self.currentFilePath.endswith('.typ'):
             QMessageBox.warning(self, "Warning", "The file is not a Typst file.")
             return
         
+        directory, file = os.path.split(self.currentFilePath)
         command = ['typst', 'compile', file]
 
         try:
             process = subprocess.Popen(command, cwd=directory)
             process.wait()
+            self.ui.statusBar.showMessage(f"Compiled Typst: {file}")
         except Exception as e:
             QMessageBox.critical(self, "Error", str(e))
 
-    def editReplace(self):
-        cursor = self.ui.textEdit.textCursor()
-        if cursor.hasSelection():
-            text, ok = QInputDialog.getText(self, 'Replace Text', 'Enter new text:')
-            if ok:
-                cursor.insertText(text)
-                self.ui.textEdit.setTextCursor(cursor)
-    
     def helpAbout(self):
-        QMessageBox.about(self, "About", "This is a software create by BFmHNO3.\n\nVersion: 1.0.0")
+        QMessageBox.about(self, "About", "This is a software created by BFmHNO3.\n\nVersion: 1.0.0")
 
-    def connectSlot(self):
-        self.ui.fileOpen.triggered.connect(self.fileOpen)
-        self.ui.fileNew.triggered.connect(self.fileNew)
-        self.ui.fileOpenFolder.triggered.connect(self.fileOpenFolder)
-        self.ui.fileNewWindow.triggered.connect(self.fileNewWindow)
-        self.ui.fileSave.triggered.connect(self.fileSave)
-        self.ui.fileSaveAs.triggered.connect(self.fileSaveAs)
-        self.ui.fileMoveTo.triggered.connect(self.fileMoveTo)
-
-        self.ui.editUndo.triggered.connect(self.ui.textEdit.undo)
-        self.ui.editRedo.triggered.connect(self.ui.textEdit.redo)
-        self.ui.editCopy.triggered.connect(self.ui.textEdit.cut)
-        self.ui.editCut.triggered.connect(self.ui.textEdit.cut)
-        self.ui.editPaste.triggered.connect(self.ui.textEdit.paste)
-        self.ui.editFind.triggered.connect(self.editFind)
-        self.ui.editJumpToTop.triggered.connect(self.editJumpToTop)
-        self.ui.editJumpToBottom.triggered.connect(self.editJumpToBottom)
-        self.ui.editJumpToSelection.triggered.connect(self.editJumpToSelection)
-        self.ui.editJumpToLineStart.triggered.connect(self.editJumpToLineStart)
-        self.ui.editJumpToLineEnd.triggered.connect(self.editJumpToLineEnd)
-        self.ui.editReplace.triggered.connect(self.editReplace)
-
-        self.ui.compileLaTeX.triggered.connect(self.compileLaTeX)
-        self.ui.complileTypst.triggered.connect(self.compileTypst)
-
-        self.ui.helpAbout.triggered.connect(self.helpAbout)
-
-        # self.ui.webEngineView.load(QUrl.fromLocalFile(filePath))
+    def closeEvent(self, event):
+        if self.ui.textEdit.toPlainText():
+            reply = QMessageBox.question(
+                self, 'Message',
+                "Are you sure to quit? Any unsaved work will be lost.",
+                QMessageBox.Yes | QMessageBox.No, QMessageBox.No
+            )
+            if reply == QMessageBox.Yes:
+                event.accept()
+            else:
+                event.ignore()
+        else:
+            event.accept()
