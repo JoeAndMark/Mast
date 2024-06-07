f46f6c4f71e1e9e672d756dea5df6bc7b52eeff2
Bill Jack <111075017+BFmHNO3@users.noreply.github.com>
2024-06-07 23:24:34 +0800

    添加应用图标

---
+++ tests/testGui.py
@@ -0,0 +1,21 @@
+"""
+模块名称：testGui.py
+模块描述：测试程序的GUI界面
+作者：JoeAndMark
+时间：2024-5-1
+"""
+
+# 导入标准库
+import sys
+
+# 导入第三方库
+
+
+# 导入自定义库
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
